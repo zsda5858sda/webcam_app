ad421dcbbddc93faf72627b28b1fbe74c4f657c7
Taehyung <10846011@ntub.edu.tw>
2021-07-07 10:22:45 +0800

    解決android上傳照片bug

--- lib/screen/image_upload.dart
+++ lib/screen/image_upload.dart
@@ -3,10 +3,10 @@ import 'dart:typed_data';
 
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:http_parser/http_parser.dart';
 import 'package:image_picker/image_picker.dart';
-import 'package:webcam_app/screen/app_bar.dart';
+import 'package:webcam_app/my_flutter_app_icons.dart';
 
 class ImagePickerPage extends StatefulWidget {
   const ImagePickerPage({Key? key}) : super(key: key);
@@ -16,36 +16,57 @@ class ImagePickerPage extends StatefulWidget {
 }
 
 class _ImagePickerPageState extends State<ImagePickerPage> {
-  List<PickedFile>? _image;
-  final picker = ImagePicker();
+  List<PickedFile>? _imageFileList;
+
+  set _imageFile(PickedFile? value) {
+    _imageFileList = value == null ? null : [value];
+  }
+
+  dynamic _pickImageError;
+  final ImagePicker _picker = ImagePicker();
   Dio dio = Dio();
-  Future _getImageFromGallery() async {
-    var image = await picker.getMultiImage();
-    setState(() {
-      _image = image;
-    });
-    final int num = _image!.length;
-    var a = 3;
-    for (int i = 0; i < num; i++) {
-      Uint8List byteData = await _image![0].readAsBytes();
-      List<int> imageData = byteData.buffer.asUint8List();
-      a = a + 1;
-      var n = a.toString();
-      MultipartFile multipartFile = MultipartFile.fromBytes(
-        imageData,
-        filename: 'work01T-A123456789-' + n + '.jpg',
-        contentType: MediaType('image', 'jpg'),
-      );
-      FormData formData = FormData.fromMap({
-        "uploaded_file": multipartFile,
-      });
-      var url = "https://vsid.ubt.ubot.com.tw:81/uploadpic";
-      var response = await dio.post(url, data: formData);
-      if (response.statusCode == 200) {
-        debugPrint("上傳成功");
-        debugPrint(multipartFile.filename);
-      } else {
-        debugPrint(response.statusMessage);
+  void _onImageButtonPressed(ImageSource source,
+      {BuildContext? context, bool isMultiImage = true}) async {
+    if (isMultiImage) {
+      try {
+        final pickedFileList = await _picker.getMultiImage();
+        setState(() {
+          _imageFileList = pickedFileList;
+        });
+        if (null != _imageFileList) {
+          final int num = _imageFileList!.length;
+          if (num > 5) {
+            errorAlert("照片最多上傳五張");
+          } else {
+            var a = 3;
+            for (int i = 0; i < num; i++) {
+              Uint8List byteData = await _imageFileList![0].readAsBytes();
+              List<int> imageData = byteData.buffer.asUint8List();
+              a = a + 1;
+              var n = a.toString();
+              MultipartFile multipartFile = MultipartFile.fromBytes(
+                imageData,
+                filename: 'work01T-A123456789-' + n + '.jpg',
+                contentType: MediaType('image', 'jpg'),
+              );
+              FormData formData = FormData.fromMap({
+                "uploaded_file": multipartFile,
+              });
+              var url = "https://vsid.ubt.ubot.com.tw:81/uploadpic";
+              var response = await dio.post(url, data: formData);
+              if (response.statusCode == 200) {
+                successAlert("上傳成功!!");
+                debugPrint(multipartFile.filename);
+              } else {
+                debugPrint(response.statusMessage);
+              }
+            }
+          }
+        }
+      } catch (e) {
+        setState(() {
+          _pickImageError = e;
+        });
       }
     }
   }
@@ -97,7 +118,13 @@ class _ImagePickerPageState extends State<ImagePickerPage> {
           FlatButton(
             minWidth: size.width * 0.5,
             height: size.height * 0.08,
-            onPressed: _getImageFromGallery,
+            onPressed: () {
+              _onImageButtonPressed(
+                ImageSource.gallery,
+                context: context,
+                isMultiImage: true,
+              );
+            },
             child: Text('上傳文件(多選)',
                 style:
                     TextStyle(color: Colors.blue, fontSize: size.width * 0.06)),
@@ -114,7 +141,7 @@ class _ImagePickerPageState extends State<ImagePickerPage> {
           FlatButton(
             minWidth: size.width * 0.5,
             height: size.height * 0.08,
-            onPressed: _getImageFromGallery,
+            onPressed: () {},
             child: Text('不上傳文件',
                 style:
                     TextStyle(color: Colors.red, fontSize: size.width * 0.06)),
@@ -126,6 +153,92 @@ class _ImagePickerPageState extends State<ImagePickerPage> {
           )
         ],
       ),
+    );
+  }
+
+  void successAlert(String message) {
+    AlertDialog dialog = AlertDialog(
+      backgroundColor: Colors.white,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(Radius.circular(15)),
+      ),
+      content: Row(
+        children: <Widget>[
+          Icon(
+            Icons.check_circle_outline,
+            color: Colors.green,
+            size: 30,
+          ),
+          Padding(padding: EdgeInsets.only(right: 10)),
+          Text(
+            message,
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 20,
+            ),
+          ),
+        ],
+      ),
+      actions: <Widget>[
+        FlatButton(
+          onPressed: () {
+            Navigator.pop(context, true);
+          },
+          child: Text(
+            "CLOSE",
+            style: TextStyle(color: Colors.black),
+          ),
+        ),
+      ],
+    );
+
+    showDialog(
+      barrierDismissible: false,
+      context: context,
+      builder: (BuildContext context) => dialog,
+    );
+  }
+
+  void errorAlert(String message) {
+    AlertDialog dialog = AlertDialog(
+      backgroundColor: Colors.white,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(Radius.circular(15)),
+      ),
+      content: Row(
+        children: <Widget>[
+          Icon(
+            Icons.cancel_rounded,
+            color: Colors.red,
+            size: 30,
+          ),
+          Padding(padding: EdgeInsets.only(right: 10)),
+          Text(
+            message,
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 20,
+            ),
+          ),
+        ],
+      ),
+      actions: <Widget>[
+        FlatButton(
+          onPressed: () {
+            Navigator.pop(context, true);
+          },
+          child: Text(
+            "CLOSE",
+            style: TextStyle(color: Colors.black),
+          ),
+        ),
+      ],
+    );
+
+    showDialog(
+      barrierDismissible: false,
+      context: context,
+      builder: (BuildContext context) => dialog,
     );
   }
 }
