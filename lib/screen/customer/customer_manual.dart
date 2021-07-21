11eda3d9a0878083388b912cc1a395821ba47cdd
azhe922 <zsda5858sda@gmail.com>
2021-07-21 11:15:06 +0800

    調整輸入框及版面

--- lib/screen/customer/customer_manual.dart
+++ lib/screen/customer/customer_manual.dart
@@ -2,9 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:webcam_app/database/dao/userDao.dart';
 import 'package:webcam_app/database/model/user.dart';
 import 'package:webcam_app/screen/component/app_bar.dart';
-import 'package:webcam_app/screen/component/button.dart';
-import 'package:webcam_app/screen/customer/photo/customer_photo.dart';
+import 'package:webcam_app/screen/customer/customer_photo.dart';
 import 'package:webcam_app/utils/response_app.dart';
+import 'package:webcam_app/utils/show_dialog_alert.dart';
 
 class CustomerMaunalScreen extends StatelessWidget {
   static const String routeName = '/maunal';
@@ -47,31 +47,48 @@ class CustomerMaunalScreen extends StatelessWidget {
                             hintText: '請輸入行員ID',
                             border: OutlineInputBorder(
                                 borderRadius: BorderRadius.all(
-                              const Radius.circular(20.0),
+                              const Radius.circular(10.0),
                             )),
                           ),
                         ),
                       ),
                       SizedBox(
-                        height: size.height * 0.07,
+                        height: size.height * 0.5,
                       ),
-                      ScreenButton(
-                          btnName: '下一步',
+                      ConstrainedBox(
+                        constraints:
+                            BoxConstraints.tightFor(width: 300, height: 50),
+                        child: ElevatedButton(
                           onPressed: () async {
                             String agentId = idController.text;
-                            String url =
-                                "https://vsid.ubt.ubot.com.tw:81/main/client/index.html?openExternalBrowser=1&agentid=$agentId";
-                            var userDao = UserDao.instance;
-                            Map<String, Object> userJson =
-                                (await userDao.readAllNotes()).first.toJson();
-                            
-                            userJson.update("webviewUrl", (value) => url);
-                            User user = User.fromJson(userJson);
-                            await userDao.update(user);
+                            if (agentId == "") {
+                              showAlertDialog(context, "", "行員ID不能為空");
+                            } else {
+                              String url =
+                                  "https://vsid.ubt.ubot.com.tw:81/main/client/index.html?openExternalBrowser=1&agentid=$agentId";
+                              var userDao = UserDao.instance;
+                              Map<String, Object> userJson =
+                                  (await userDao.readAllNotes()).first.toJson();
 
-                            Navigator.pushNamed(
-                                context, CustomerPhotoScreen.routeName);
-                          })
+                              userJson.update("webviewUrl", (value) => url);
+                              User user = User.fromJson(userJson);
+                              await userDao.update(user);
+
+                              Navigator.pushNamed(
+                                  context, CustomerPhotoScreen.routeName);
+                            }
+                          },
+                          child: RichText(
+                            text: TextSpan(
+                                style: TextStyle(fontSize: 20), text: '下一步'),
+                          ),
+                        ),
+                        // child: ScreenButton(
+                        //     btnName: '下一步',
+                        //     onPressed: () async {
+
+                        //     }),
+                      ),
                     ],
                   ),
                 ),
