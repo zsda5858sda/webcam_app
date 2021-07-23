de532d110210bf69b284c5f268a7ad4db041864d
Taehyaung <10846011@ntub.edu.tw>
2021-07-23 11:20:55 +0800

    webview抓完影片後套用背景上傳

--- lib/screen/customer/customer_meet.dart
+++ lib/screen/customer/customer_meet.dart
@@ -1,11 +1,116 @@
+import 'dart:convert';
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:flutter_uploader/flutter_uploader.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:url_launcher/url_launcher.dart';
+import 'package:gallery_saver/gallery_saver.dart';
+
+FlutterUploader _uploader = FlutterUploader();
+
+void backgroundHandler() {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Notice these instances belong to a forked isolate.
+  var uploader = FlutterUploader();
+
+  var notifications = FlutterLocalNotificationsPlugin();
+
+  // Only show notifications for unprocessed uploads.
+  SharedPreferences.getInstance().then((preferences) {
+    var processed = preferences.getStringList('processed') ?? <String>[];
+
+    if (Platform.isAndroid) {
+      uploader.progress.listen((progress) {
+        if (processed.contains(progress.taskId)) {
+          return;
+        }
+
+        notifications.show(
+          progress.taskId.hashCode,
+          'FlutterUploader Example',
+          'Upload in Progress',
+          NotificationDetails(
+            android: AndroidNotificationDetails(
+              'FlutterUploader.Example',
+              'FlutterUploader',
+              'Installed when you activate the Flutter Uploader Example',
+              progress: progress.progress ?? 0,
+              icon: 'ic_upload',
+              enableVibration: false,
+              importance: Importance.low,
+              showProgress: true,
+              onlyAlertOnce: true,
+              maxProgress: 100,
+              channelShowBadge: false,
+            ),
+            iOS: IOSNotificationDetails(),
+          ),
+        );
+      });
+    }
+
+    uploader.result.listen((result) {
+      if (processed.contains(result.taskId)) {
+        return;
+      }
+
+      processed.add(result.taskId);
+      preferences.setStringList('processed', processed);
+
+      notifications.cancel(result.taskId.hashCode);
+
+      final successful = result.status == UploadTaskStatus.complete;
+
+      var title = 'Upload Complete';
+      if (result.status == UploadTaskStatus.failed) {
+        title = 'Upload Failed';
+      } else if (result.status == UploadTaskStatus.canceled) {
+        title = 'Upload Canceled';
+      }
+
+      notifications
+          .show(
+        result.taskId.hashCode,
+        'FlutterUploader Example',
+        title,
+        NotificationDetails(
+          android: AndroidNotificationDetails(
+            'FlutterUploader.Example',
+            'FlutterUploader',
+            'Installed when you activate the Flutter Uploader Example',
+            icon: 'ic_upload',
+            enableVibration: !successful,
+            importance: result.status == UploadTaskStatus.failed
+                ? Importance.high
+                : Importance.min,
+          ),
+          iOS: IOSNotificationDetails(
+            presentAlert: true,
+          ),
+        ),
+      )
+          .catchError((e, stack) {
+        print('error while showing notification: $e, $stack');
+      });
+    });
+  });
+}
 
 class CustomerPage extends StatefulWidget {
-  dynamic data;
-  CustomerPage(this.data);
+  // dynamic data;
+  // CustomerPage(this.data);
+  const CustomerPage({
+    Key? key,
+    required this.uploader,
+    required this.uploadURL,
+  }) : super(key: key);
+
+  final FlutterUploader uploader;
+  final Uri uploadURL;
   @override
   _CustomerPage createState() => new _CustomerPage();
 }
@@ -16,9 +121,10 @@ class _CustomerPage extends State<CustomerPage> {
   InAppWebViewController? webViewController;
   InAppWebViewGroupOptions options = InAppWebViewGroupOptions(
       crossPlatform: InAppWebViewOptions(
-        useShouldOverrideUrlLoading: true,
-        mediaPlaybackRequiresUserGesture: false,
-      ),
+          useOnDownloadStart: true,
+          useShouldOverrideUrlLoading: true,
+          mediaPlaybackRequiresUserGesture: false,
+          javaScriptEnabled: true),
       android: AndroidInAppWebViewOptions(
         useHybridComposition: true,
       ),
@@ -34,7 +140,7 @@ class _CustomerPage extends State<CustomerPage> {
   @override
   void initState() {
     super.initState();
-
+    _uploader.setBackgroundHandler(backgroundHandler);
     pullToRefreshController = PullToRefreshController(
       options: PullToRefreshOptions(
         color: Colors.blue,
@@ -57,10 +163,21 @@ class _CustomerPage extends State<CustomerPage> {
 
   @override
   Widget build(BuildContext context) {
-    String url =
-        "https://vsid.ubt.ubot.com.tw:81/main/client/index.html?openExternalBrowser=1&agentid=";
-    String agentId = widget.data[0].toString();
-    String finalUrl = url + agentId;
+    var downloadUrl;
+    String url = "https://172.20.10.10:82/main/Login.html";
+    // String agentId = widget.data[0].toString();
+    String finalUrl = url;
+    var now = DateTime.now();
+    var year = now.year.toString();
+    var month =
+        now.month < 10 ? "0" + now.month.toString() : now.month.toString();
+    var day = now.day < 10 ? "0" + now.day.toString() : now.day.toString();
+    var hour = now.hour < 10 ? "0" + now.hour.toString() : now.hour.toString();
+    var min =
+        now.minute < 10 ? "0" + now.minute.toString() : now.minute.toString();
+    var sec =
+        now.second < 10 ? "0" + now.second.toString() : now.second.toString();
+    var datetime = year + month + day + hour + min + sec;
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
@@ -76,12 +193,32 @@ class _CustomerPage extends State<CustomerPage> {
                 pullToRefreshController: pullToRefreshController,
                 onWebViewCreated: (controller) {
                   webViewController = controller;
+                  controller.addJavaScriptHandler(
+                    handlerName: 'blobToBase64Handler',
+                    callback: (data) async {
+                      if (data.isNotEmpty) {
+                        final String receivedFileInBase64 = data[0];
+                        final String receivedMimeType = data[1];
+                        // NOTE: create a method that will handle your extensions
+                        final String? yourExtension = "webm";
+                        _createFileFromBase64(receivedFileInBase64,
+                            '0000915-A123456789-b-' + datetime, yourExtension!);
+                      } else {
+                        debugPrint('data is empty!!');
+                      }
+                    },
+                  );
                 },
                 onLoadStart: (controller, url) {
                   setState(() {
                     this.url = url.toString();
                     urlController.text = this.url;
                   });
+                },
+                onReceivedServerTrustAuthRequest:
+                    (controller, challenge) async {
+                  return ServerTrustAuthResponse(
+                      action: ServerTrustAuthResponseAction.PROCEED);
                 },
                 androidOnPermissionRequest:
                     (controller, origin, resources) async {
@@ -99,7 +236,8 @@ class _CustomerPage extends State<CustomerPage> {
                     "chrome",
                     "data",
                     "javascript",
-                    "about"
+                    "about",
+                    "blob:"
                   ].contains(uri!.scheme)) {
                     if (await canLaunch(url)) {
                       // Launch the App
@@ -138,8 +276,16 @@ class _CustomerPage extends State<CustomerPage> {
                     urlController.text = this.url;
                   });
                 },
-                onConsoleMessage: (controller, consoleMessage) {
+                onDownloadStart: (controller, url) async {
+                  print("onDownloadStart $url");
+                },
+                onConsoleMessage: (controller, consoleMessage) async {
                   print(consoleMessage);
+                  if (consoleMessage.message.contains('blob')) {
+                    setState(() {
+                      downloadUrl = consoleMessage.message;
+                    });
+                  }
                 },
               ),
               progress < 1.0
@@ -149,6 +295,53 @@ class _CustomerPage extends State<CustomerPage> {
           ),
         ),
       ]))),
+    );
+  }
+
+  _createFileFromBase64(
+      String base64content, String fileName, String yourExtension) async {
+    var bytes = base64Decode(base64content.replaceAll('\n', ''));
+    debugPrint(bytes.toString());
+    final output;
+    if (Platform.isAndroid) {
+      output = await getExternalStorageDirectory();
+    } else {
+      output = await getApplicationDocumentsDirectory();
+    }
+    final file = File("${output!.path}/$fileName.$yourExtension");
+    await file.writeAsBytes(bytes.buffer.asUint8List());
+    print("${output.path}/${fileName}.webm");
+    // await GallerySaver.saveVideo(file.path);
+    _handleFileUpload([file.path]);
+    // File(file.path).deleteSync();
+    setState(() {});
+  }
+
+  void _handleFileUpload(List<String> paths) async {
+    final prefs = await SharedPreferences.getInstance();
+    final binary = prefs.getBool('binary') ?? false;
+    await widget.uploader.enqueue(_buildUpload(
+      binary,
+      paths.whereType<String>().toList(),
+    ));
+  }
+
+  Upload _buildUpload(bool binary, List<String> paths) {
+    final tag = 'upload';
+
+    var url = widget.uploadURL;
+
+    return MultipartFormDataUpload(
+      url: url.toString(),
+      data: {'name': 'john'},
+      files: paths
+          .map((e) => FileItem(
+                path: e,
+                field: 'videofile',
+              ))
+          .toList(),
+      method: UploadMethod.POST,
+      tag: tag,
     );
   }
 }
