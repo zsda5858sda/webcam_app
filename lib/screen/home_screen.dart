9b695477b7d2d18daf934d2aee838fa054dae182
azhe922 <zsda5858sda@gmail.com>
2021-07-26 14:22:51 +0800

    修正推播跳頁後相機之錯誤

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -6,10 +6,11 @@ import 'package:webcam_app/database/model/user.dart';
 import 'package:webcam_app/screen/clerk/clerk_login.dart';
 import 'package:webcam_app/screen/component/app_bar.dart';
 import 'package:webcam_app/screen/component/button.dart';
+import 'package:webcam_app/screen/customer/customer_meet.dart';
+import 'package:webcam_app/screen/customer/customer_photo_doc.dart';
 import 'package:webcam_app/screen/customer/customer_register.dart';
 import 'package:webcam_app/screen/customer/customer_options.dart';
 import 'package:webcam_app/screen/customer/customer_photo.dart';
-import 'package:webcam_app/screen/customer/customer_meet.dart';
 import 'package:webcam_app/utils/response_app.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
@@ -30,34 +31,20 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   @override
-  Widget build(BuildContext context) {
+  void initState() {
+    super.initState();
     _configureSelectNotificationSubject();
-    final flutterLocalNotificationsPlugin =
-        widget.flutterLocalNotificationsPlugin;
-    final channel = widget.channel;
-    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
-      RemoteNotification? notification = message.notification;
-      flutterLocalNotificationsPlugin.show(
-          notification.hashCode,
-          notification!.title,
-          notification.body,
-          NotificationDetails(
-            android: AndroidNotificationDetails(
-              channel.id,
-              channel.name,
-              channel.description,
-              icon: 'launch_background',
-            ),
-          ),
-          payload: message.data['url']);
-    });
+    _configureFirebaseMessage();
+  }
 
-    // 背景監聽消息
-    FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
-      print("在背景執行時收到訊息");
-      Navigator.pushNamed(context, CustomerOptionsScreen.routeName);
-    });
+  @override
+  void dispose() {
+    selectNotificationSubject.close();
+    super.dispose();
+  }
 
+  @override
+  Widget build(BuildContext context) {
     return Builder(builder: (context) {
       ResponsiveApp.setMq(context);
       return WillPopScope(
@@ -105,14 +92,50 @@ class _HomeScreenState extends State<HomeScreen> {
 
   void _configureSelectNotificationSubject() {
     selectNotificationSubject.stream.listen((String? payload) async {
-      var userDao = UserDao.instance;
-      Map<String, Object> userJson =
-          (await userDao.readAllNotes()).first.toJson();
-
-      userJson.update("webviewUrl", (value) => payload!);
-      User user = User.fromJson(userJson);
-      await userDao.update(user);
-      await Navigator.pushNamed(context, CustomerPhotoScreen.routeName);
+      await updateUrl(payload!);
+      Navigator.pushNamed(context, CustomerPhotoScreen.routeName);
     });
+  }
+
+  void _configureFirebaseMessage() {
+    final flutterLocalNotificationsPlugin =
+        widget.flutterLocalNotificationsPlugin;
+    final channel = widget.channel;
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      RemoteNotification? notification = message.notification;
+      AndroidNotification? android = message.notification?.android;
+      if (notification != null && android != null && !kIsWeb) {
+        flutterLocalNotificationsPlugin.show(
+            notification.hashCode,
+            notification.title,
+            notification.body,
+            NotificationDetails(
+              android: AndroidNotificationDetails(
+                channel.id,
+                channel.name,
+                channel.description,
+                icon: 'launch_background',
+              ),
+            ),
+            payload: message.data['url']);
+      }
+    });
+
+    // 背景監聽消息
+    FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
+      print("在背景執行時收到訊息");
+      updateUrl(message.data['url']);
+      Navigator.pushNamed(context, CustomerPhotoScreen.routeName);
+    });
+  }
+
+  Future<void> updateUrl(String payload) async {
+    var userDao = UserDao.instance;
+    Map<String, Object> userJson =
+        (await userDao.readAllNotes()).first.toJson();
+
+    userJson.update("webviewUrl", (value) => payload);
+    User user = User.fromJson(userJson);
+    await userDao.update(user);
   }
 }
