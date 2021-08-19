edd3e23fee51c54f7e4f5b667aaed628f5e8a623
azhe922 <zsda5858sda@gmail.com>
2021-08-19 15:55:27 +0800

    調整fcm server key

--- lib/config/config.dart
+++ lib/config/config.dart
@@ -5,7 +5,7 @@ class Config {
   static final String FCM_VAPID_KEY =
       "BOa5kaeCiN4BmjKzLsitUL6p2lMY3KJoJx2ksW2pMnEw61lDEAHdqGTSAYkoFGESIzJDtiOKsycj3JIrTUL-yh4";
   static final String FCM_SERVER_KEY =
-      "AAAAtOu4HKI:APA91bGbePL8rBTNMFpd58JbwVz6D-QtZYofXs7GDIOskYsP8LFTXERzGcoktA9hdLnGkfoVBowmwst_MPUJkD_SBamkLflf9gF_VimT5pFtEny1NIghvPAhzrYn1Go1zywd47I3yuTw";
+      "AAAAeQeiCcc:APA91bE-CQUwMHTm6_KXzQ7Q7Y2uEYIz7Cxdfz6nVPkZqIMmWO6Wk6F1UOCMiQ9c8n_O-rJqRVDKDeFWGXMJU2KiN5dSA9ZWXEiWFa4b3DVRWk6Sx6WDwha8IiGbjKz792OaybbIahBY";
   static final String WEBRTC_URL =
       "https://vsid.ubt.ubot.com.tw:81/main/client/index.html?openExternalBrowser=1";
 }
