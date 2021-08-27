578a9e7d79a4ca20637704f74907141387124180
Taehyaung <10846011@ntub.edu.tw>
2021-08-27 11:10:12 +0800

    行員登入畫面微調，新增部門選單

--- lib/screen/clerk/clerk_login.dart
+++ lib/screen/clerk/clerk_login.dart
@@ -1,6 +1,9 @@
+import 'dart:convert';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_uploader/flutter_uploader.dart';
+import 'package:webcam_app/config/config.dart';
 import 'package:webcam_app/database/dao/clerkDao.dart';
 import 'package:webcam_app/database/model/clerk.dart';
 import 'package:webcam_app/screen/component/banner_pic.dart';
@@ -14,8 +17,11 @@ import 'package:webcam_app/utils/fcm_service.dart';
 import 'package:webcam_app/utils/hbcode.dart';
 import 'package:webcam_app/utils/http_utils.dart';
 import 'package:webcam_app/utils/responsive_app.dart';
+import 'package:webcam_app/utils/log.dart';
+import 'package:http/http.dart' as http;
 import 'package:webcam_app/utils/show_dialog_alert.dart';
 import 'package:select_form_field/select_form_field.dart';
+import 'package:wifi_info_flutter/wifi_info_flutter.dart';
 
 import 'admin_screen.dart';
 
@@ -46,36 +52,10 @@ class _ClerkLoginScreenState extends State<ClerkLoginScreen> {
   Size size = ResponsiveApp().mq.size;
   bool isLogin = true;
   String? labelText;
-  final List<Map<String, dynamic>> _items = [
-    {
-      'value': 900,
-      'label': '消費金融部',
-      'icon': Icon(Icons.stop),
-    },
-    {
-      'value': 903,
-      'label': '車輛貸款部',
-      'icon': Icon(Icons.fiber_manual_record),
-    },
-    {
-      'value': 905,
-      'label': '保險代理部',
-      'icon': Icon(Icons.fiber_manual_record),
-    },
-    {
-      'value': 907,
-      'label': '理財貸款部',
-      'icon': Icon(Icons.fiber_manual_record),
-    },
-    {
-      'value': 202,
-      'label': '企業金融部',
-      'icon': Icon(Icons.fiber_manual_record),
-    },
-  ];
-
+  final List<Map<String, dynamic>> _items = [];
   @override
   void initState() {
+    _getIPAddress();
     super.initState();
   }
 
@@ -125,10 +105,13 @@ class _ClerkLoginScreenState extends State<ClerkLoginScreen> {
                                 ),
                                 SmallTitle(title: "請輸入員工編號"),
                                 InputBox(
-                                    size: size,
-                                    idController: idController,
-                                    showText: true,
-                                    HintText: "請輸入員工編號"),
+                                  size: size,
+                                  idController: idController,
+                                  showText: true,
+                                  HintText: "請輸入員工編號",
+                                  isCorrect: true,
+                                  verify: () {},
+                                ),
                                 SizedBox(
                                   height: size.height * 0.01,
                                 ),
@@ -138,6 +121,8 @@ class _ClerkLoginScreenState extends State<ClerkLoginScreen> {
                                   idController: passwordController,
                                   HintText: "請輸入密碼",
                                   showText: false,
+                                  isCorrect: true,
+                                  verify: () {},
                                 ),
                                 SizedBox(
                                   height: size.height * 0.01,
@@ -211,108 +196,161 @@ class _ClerkLoginScreenState extends State<ClerkLoginScreen> {
                             )),
                       ),
                       Visibility(
-                        visible: !isLogin,
-                        child: Container(
-                            margin: EdgeInsets.only(left: 15, right: 15),
-                            height: size.height * 0.37,
-                            decoration: BoxDecoration(
-                                color: Colors.white.withOpacity(0.8),
-                                borderRadius: BorderRadius.only(
-                                    topLeft: Radius.circular(30),
-                                    topRight: Radius.circular(30))),
-                            child: Column(
-                              children: <Widget>[
-                                SizedBox(
-                                  height: size.height * 0.02,
-                                ),
-                                SmallTitle(title: "請輸入顧客電話號碼"),
-                                InputBox(
-                                    showText: true,
-                                    size: size,
-                                    idController: phoneController,
-                                    HintText: "請輸入顧客電話號碼"),
-                                SizedBox(
-                                  height: size.height * 0.01,
-                                ),
-                                Container(
-                                    height: 40,
-                                    width: size.width * 0.6,
-                                    child: SelectFormField(
-                                      type: SelectFormFieldType
-                                          .dropdown, // or can be dialog
-                                      initialValue: 'circle',
-                                      labelText:
-                                          labelText == null ? "消費金融部" : null,
-                                      items: _items,
-                                      onChanged: (val) {
-                                        setState(() {
-                                          labelText = val;
-                                          print(labelText);
-                                        });
-                                      },
-                                      onSaved: (val) => print(val),
-                                    )),
-                                SizedBox(
-                                  height: size.height * 0.04,
-                                ),
-                                RequestBtn(
-                                  size: size,
-                                  btnName: "發送推播",
-                                  onPress: () async {
-                                    String phone = phoneController.text;
-                                    List<Clerk> clerk =
-                                        await ClerkDao.instance.readAllNotes();
-                                    try {
-                                      var response =
-                                          await HttpUtils().getToken(phone);
-                                      String token = response["data"];
-                                      String message = response["message"];
-                                      HttpUtils().sendLog(
-                                          clerk.first.account.toString(),
-                                          message,
-                                          "1");
-                                      print(token);
-                                      print(clerk.first.account.toString());
-                                      print("---------------");
-                                      FCMService.sendToCustomer(
-                                          token, clerk.first.account);
-                                      showAlertDialog(context, "推播成功", "已傳送通知");
-                                    } on Exception catch (error) {
-                                      await HttpUtils().sendLog(
-                                          clerk.first.account.toString(),
-                                          error.toString(),
-                                          "1");
-                                      showAlertDialog(
-                                          context, "推播失敗", error.toString());
-                                    }
-                                  },
-                                ),
-                                SizedBox(
-                                  height: size.height * 0.03,
-                                ),
-                                RequestBtn(
-                                  size: size,
-                                  btnName: "進行視訊",
-                                  onPress: () => Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                        builder: (context) => ClerkWebRTC(
-                                              deparment: labelText.toString(),
-                                              agentId: "0000915",
-                                              uploader: _uploader,
-                                              uploadURL: uploadVideoUrl,
-                                              webRtcUrl: Uri.parse(webRtcUrl +
-                                                  idController.text),
+                          visible: !isLogin,
+                          child: FutureBuilder(
+                            builder: (context, AsyncSnapshot snapshot) {
+                              if (_items != null) {
+                                return Container(
+                                    margin:
+                                        EdgeInsets.only(left: 15, right: 15),
+                                    height: size.height * 0.37,
+                                    decoration: BoxDecoration(
+                                        color: Colors.white.withOpacity(0.8),
+                                        borderRadius: BorderRadius.only(
+                                            topLeft: Radius.circular(30),
+                                            topRight: Radius.circular(30))),
+                                    child: Column(
+                                      children: <Widget>[
+                                        SizedBox(
+                                          height: size.height * 0.02,
+                                        ),
+                                        SmallTitle(title: "請輸入顧客電話號碼"),
+                                        InputBox(
+                                            isCorrect: true,
+                                            verify: () {},
+                                            showText: true,
+                                            size: size,
+                                            idController: phoneController,
+                                            HintText: "請輸入顧客電話號碼"),
+                                        SizedBox(
+                                          height: size.height * 0.01,
+                                        ),
+                                        Container(
+                                            height: 40,
+                                            width: size.width * 0.6,
+                                            child: SelectFormField(
+                                              type: SelectFormFieldType
+                                                  .dropdown, // or can be dialog
+                                              items: _items,
+                                              labelText: labelText == null
+                                                  ? _items[0]['label']
+                                                  : null,
+                                              onChanged: (val) {
+                                                setState(() {
+                                                  labelText = val;
+                                                  print(labelText);
+                                                });
+                                              },
+                                              onSaved: (val) => print(val),
                                             )),
-                                  ),
-                                )
-                              ],
-                            )),
-                      )
+                                        SizedBox(
+                                          height: size.height * 0.04,
+                                        ),
+                                        RequestBtn(
+                                          size: size,
+                                          btnName: "發送推播",
+                                          onPress: () async {
+                                            String phone = phoneController.text;
+                                            List<Clerk> clerk = await ClerkDao
+                                                .instance
+                                                .readAllNotes();
+                                            try {
+                                              var response = await HttpUtils()
+                                                  .getToken(phone);
+                                              String token = response["data"];
+                                              String message =
+                                                  response["message"];
+                                              HttpUtils().sendLog(
+                                                  clerk.first.account
+                                                      .toString(),
+                                                  message,
+                                                  "1");
+                                              print(token);
+                                              print(clerk.first.account
+                                                  .toString());
+                                              print("---------------");
+                                              FCMService.sendToCustomer(
+                                                  token, clerk.first.account);
+                                              showAlertDialog(
+                                                  context, "推播成功", "已傳送通知");
+                                            } on Exception catch (error) {
+                                              await HttpUtils().sendLog(
+                                                  clerk.first.account
+                                                      .toString(),
+                                                  error.toString(),
+                                                  "1");
+                                              showAlertDialog(context, "推播失敗",
+                                                  error.toString());
+                                            }
+                                          },
+                                        ),
+                                        SizedBox(
+                                          height: size.height * 0.03,
+                                        ),
+                                        RequestBtn(
+                                            size: size,
+                                            btnName: "進行視訊",
+                                            onPress: () {
+                                              if (labelText == null) {
+                                                print(_items[0]['value']);
+                                              } else {
+                                                print(labelText);
+                                              }
+                                              Navigator.push(
+                                                context,
+                                                MaterialPageRoute(
+                                                    builder: (context) =>
+                                                        ClerkWebRTC(
+                                                          deparment:
+                                                              labelText == null
+                                                                  ? _items[0]
+                                                                      ['value']
+                                                                  : labelText,
+                                                          agentId:
+                                                              idController.text,
+                                                          uploader: _uploader,
+                                                          uploadURL:
+                                                              uploadVideoUrl,
+                                                          webRtcUrl: Uri.parse(
+                                                              webRtcUrl +
+                                                                  idController
+                                                                      .text),
+                                                        )),
+                                              );
+                                            })
+                                      ],
+                                    ));
+                              } else {
+                                return SizedBox(
+                                  child: CircularProgressIndicator(),
+                                  width: 60,
+                                  height: 60,
+                                );
+                              }
+                            },
+                          ))
                     ],
                   )
                 ],
               ))),
     );
+  }
+
+  Future _getIPAddress() async {
+    String result;
+    try {
+      var response = await http.get(Uri.parse(Config.WORKTYPE_URL)); //2
+      Map<String, dynamic> json = jsonDecode(response.body);
+      List<dynamic> data = json["data"];
+      for (int i = 0; i < data.length; i++) {
+        _items
+            .add({'value': data[i]['workType'], 'label': data[i]['workName']});
+      }
+      print(data);
+      print(_items);
+    } catch (exception) {
+      result = exception.toString();
+      print("I am exception" + result);
+    }
   }
 }
